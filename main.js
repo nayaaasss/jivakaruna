d26f8ca7cc4c52c82ae1505d4e7c9cf47c19ac06
nayaaasss <widamud0323@gmail.com>
2024-05-08 15:12:52 +0700

    website jivakaruna

---
+++ main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+  });
+
+//scrol
+const animate = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: "2500",
+    delay: "400"
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", {origin: "left"});
+animate.reveal(".home-img", {origin: "bottom"});
+animate.reveal(".ser-box, .product-box, .team-box, .book-data", {interval: 100});
